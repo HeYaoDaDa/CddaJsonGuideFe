d6eae953cf7fa7c598e00c6214f23f022cefc9b1
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-03-20 22:54:28 +0800

    fix ReproductionCard

--- src/components/jsonItem/ReproductionCard.vue
+++ src/components/jsonItem/ReproductionCard.vue
@@ -1,17 +1,13 @@
 <template>
   <q-card class="col q-my-sm q-mx-xs" v-if="reproductionCard">
     <q-card-section>
-      <p class="text-subtitle1 text-weight-bold">
+      <p class="text-subtitle1 text-weight-bold" v-if="babyMonster">
         {{ $t('label.baby_monster') }}:
-        <span class="text-body2 text-weight-regular">{{
-          reproductionCard.reproduction.baby_monster
-        }}</span>
+        <span class="text-body2 text-weight-regular">{{ babyMonster }}</span>
       </p>
-      <p class="text-subtitle1 text-weight-bold">
+      <p class="text-subtitle1 text-weight-bold" v-if="babyEgg">
         {{ $t('label.baby_egg') }}:
-        <span class="text-body2 text-weight-regular">{{
-          reproductionCard.reproduction.baby_egg
-        }}</span>
+        <span class="text-body2 text-weight-regular">{{ babyEgg }}</span>
       </p>
       <p class="text-subtitle1 text-weight-bold">
         {{ $t('label.baby_count') }}:
@@ -40,6 +36,8 @@
 <script lang="ts">
 import { ref } from 'vue';
 import { ReproductionCardClass } from 'src/cards/monsters/ReproductionCard';
+import { getJsonItem } from 'src/api/jsonItem';
+import { getName } from 'src/utils/JsonItemUtil';
 
 export default {
   name: 'ReproductionCard',
@@ -54,4 +52,22 @@ const props = defineProps<{
 }>();
 const reproductionCard = ref(props.reproductionCard);
 console.debug('reproductionCard is read, value is ', reproductionCard.value);
+
+const babyMonster = ref(props.reproductionCard.reproduction?.baby_monster);
+const babyEgg = ref(props.reproductionCard.reproduction?.baby_egg);
+
+if (babyMonster.value) {
+  void getJsonItem('monster', babyMonster.value).then((jsonItem) => {
+    if (jsonItem) {
+      babyMonster.value = getName(jsonItem);
+    }
+  });
+}
+if (babyEgg.value) {
+  void getJsonItem('item', babyEgg.value).then((jsonItem) => {
+    if (jsonItem) {
+      babyEgg.value = getName(jsonItem);
+    }
+  });
+}
 </script>
