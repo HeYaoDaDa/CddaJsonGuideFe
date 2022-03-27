5ccb208e83e9ddd98ae000311ac37278a4b4f8b5
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-03-27 15:24:13 +0800

    remove old card

--- src/components/JsonItemComponent.vue
+++ src/components/JsonItemComponent.vue
@@ -3,8 +3,6 @@
     <name-card :jsonItem="jsonItem" />
     <general-card :jsonItem="jsonItem" />
     <flags-card :jsonItem="jsonItem" />
-    <attack-card :jsonItem="jsonItem" />
-    <armor-card :jsonItem="jsonItem" />
     <card-renders />
     <json-card :jsonItem="jsonItem" />
   </template>
@@ -22,9 +20,7 @@ export default {
 import NameCard from 'src/components/jsonItem/NameCard.vue';
 import GeneralCard from 'src/components/jsonItem/GeneralCard.vue';
 import FlagsCard from 'src/components/jsonItem/FlagsCard.vue';
-import AttackCard from 'src/components/jsonItem/AttackCard.vue';
 import JsonCard from 'src/components/jsonItem/JsonCard.vue';
-import ArmorCard from 'src/components/jsonItem/ArmorCard.vue';
 import { featureFactorys } from 'src/features';
 import { ref, VNode, h } from 'vue';
 import CardComponent from 'src/components/jsonItem/CardComponent.vue';
