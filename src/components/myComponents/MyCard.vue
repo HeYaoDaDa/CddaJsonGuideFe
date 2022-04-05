35e0e954048f689bf9c8850d9f4b56c580eb5cdf
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-04-05 18:48:46 +0800

    add MyCard and MyField

---
+++ src/components/myComponents/MyCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-card class="col q-my-sm q-mx-xs" v-if="isShow">
+    <q-card-section>
+      <p v-if="route">{{ $t(label) }}</p>
+      <router-link v-else>{{ $t(label) }}</router-link>
+      <dl>
+        <slot></slot>
+      </dl>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { toRefs } from 'vue';
+import { RouteLocationRaw } from 'vue-router';
+export default {
+  name: 'MyCard',
+  inheritAttrs: false,
+  customOptions: {},
+};
+</script>
+
+<script setup lang="ts">
+const { label, route } = toRefs(
+  defineProps<{
+    label: string;
+    route?: RouteLocationRaw;
+  }>()
+);
+</script>
+
+<style lang="scss">
+dl {
+  display: grid;
+  grid-template-columns: max-content auto;
+}
+</style>
---
+++ src/components/myComponents/MyField.vue
@@ -0,0 +1,28 @@
+<template>
+  <dt>
+    <p v-if="route">{{ $t(label) }}</p>
+    <router-link v-else>{{ $t(label) }}</router-link>
+  </dt>
+  <dd>
+    <slot></slot>
+  </dd>
+</template>
+
+<script lang="ts">
+import { toRefs } from 'vue';
+import { RouteLocationRaw } from 'vue-router';
+export default {
+  name: 'MyField',
+  inheritAttrs: false,
+  customOptions: {},
+};
+</script>
+
+<script setup lang="ts">
+const { label, route } = toRefs(
+  defineProps<{
+    label: string;
+    route?: RouteLocationRaw;
+  }>()
+);
+</script>
